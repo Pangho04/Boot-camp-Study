6384966a8542a3aab83a256a6c8d9a386eea8845
Pangho04 <dlckdgh456@gmail.com>
2024-05-09 23:21:27 +0900

    최수종 찾기 게임 now making...

---
+++ 부트캠프 사전학습/프리코스_3주차_이창호/homeworks/2_array.js
@@ -0,0 +1,135 @@
+(function () {
+  "use strict";
+
+  describe("배열 다루기 📶", function () {
+    describe("코드 에디터에서 homeworks/2_array.js 파일을 열어주세요.", function () {
+      /*
+       *
+       * 단순히 정답을 맞추는 것이 아닌, 이유를 정확히 아는 것이 가장 중요합니다. 💁🏻‍♂️
+       *
+       */
+
+      it("배열 만들기 #1", function () {
+        const result = [1, 2, 3];
+        const guess = [1, 2, 3];
+
+        expect(result).to.eql(guess);
+      });
+
+      it("배열 비우기 #2", function () {
+        const result = [];
+
+        let guess = [1, 2, 3];
+        guess = [];
+
+        expect(result).to.eql(guess);
+      });
+
+      it("배열 비우기 #3", function () {
+        const result = [];
+
+        const guess = [1, 2, 3];
+        guess.length = 0;
+
+        expect(result).to.eql(guess);
+      });
+
+      it("배열 비우기 #4", function () {
+        const result = [];
+
+        const guess = [1, 2, 3];
+        guess.splice(0, guess.length);
+
+        expect(result).to.eql(guess);
+      });
+
+      it("배열 요소 추가하기", function () {
+        const result = [1, 2, 3, 4];
+
+        const guess = [1, 2, 3];
+        guess.push(4);
+
+        expect(result).to.eql(guess);
+      });
+
+      it("배열 요소 추가하기", function () {
+        const result = [1, 2, 3];
+        result.unshift(5);
+
+        const guess = [5, 1, 2, 3];
+
+        expect(result).to.eql(guess);
+      });
+
+      it("배열 요소 제거하기", function () {
+        const result = [1, 2, 3];
+        result.pop();
+
+        const guess = [1, 2];
+
+        expect(result).to.eql(guess);
+      });
+
+      it("배열 요소 제거하기", function () {
+        const result = [1, 2, 3];
+        result.shift();
+
+        const guess = [2, 3];
+
+        expect(result).to.eql(guess);
+      });
+
+      it("배열에서 원하는 요소 인덱스 찾기", function () {
+        const arr = [1, 2, 3, 4, 5, 2, 10, 13, 2];
+        const result = arr.indexOf(2, 2);
+        const guess = 5;
+
+        expect(result).to.eql(guess);
+      });
+
+      it("배열에서 원하는 요소 찾기", function () {
+        const arr = [1, 2, 3, 4, 5, 2, 10, 13, 2];
+        const result = arr.includes(2, 2);
+        const guess = true;
+
+        expect(result).to.eql(guess);
+      });
+
+      it("배열 합치기", function () {
+        const arr1 = [1, 2, 3];
+        const arr2 = [1, 4, 2];
+        const result = arr1.concat(arr2);
+
+        const guess = [1, 2, 3, 1, 4, 2];
+
+        expect(result).to.eql(guess);
+      });
+
+      it("배열 여부 확인하기", function () {
+        const arr = [1, 2, 3];
+        const result = Array.isArray(arr);
+        const guess = true;
+
+        expect(result).to.eql(guess);
+      });
+
+      it("배열 요소 문자열로 만들기", function () {
+        const arr = ["010", 1234, 5678];
+        const result = arr.join("-");
+
+        const guess = "010-1234-5678";
+
+        expect(result).to.eql(guess);
+      });
+
+      it("배열 뒤집기", function () {
+        const arr = ["셋", "둘", "하나"];
+        const result = arr.reverse();
+
+        const guess = ["하나", "둘", "셋"];
+
+        expect(result).to.eql(guess);
+      });
+    });
+  });
+})();
---
+++ 부트캠프 사전학습/프리코스_3주차_이창호/homeworks/4_object.js
@@ -0,0 +1,180 @@
+(function () {
+  "use strict";
+
+  describe("객체 다루기 🔠", function () {
+    describe("코드 에디터에서 homeworks/4_object.js 파일을 열어주세요.", function () {
+      /*
+       *
+       * 단순히 정답을 맞추는 것이 아닌, 이유를 정확히 아는 것이 가장 중요합니다. 💁🏻‍♂️
+       *
+       */
+
+      it("객체 데이터 다루기 #1", function () {
+        const person = {
+          name: "leo",
+          job: "operations manager",
+        };
+
+        person.years = 3;
+        person.job = "pm";
+
+        const result = person.job;
+        const guess = "pm";
+
+        expect(result).to.eql(guess);
+      });
+
+      it("객체 데이터 다루기 #2", function () {
+        const person = {
+          name: "leo",
+        };
+
+        person.name = "dd";
+        person.job = "designer";
+
+        const result = person.name;
+        const guess = "dd";
+
+        expect(result).to.eql(guess);
+      });
+
+      it("객체 데이터 다루기 #3", function () {
+        const employee = {
+          name: "dm",
+          job: "software engineer",
+          years: 1,
+        };
+
+        const key = "id";
+        employee[key] = 4;
+
+        const result = employee.id;
+        const guess = 4;
+
+        expect(result).to.eql(guess);
+      });
+
+      it("객체 데이터 다루기 #4", function () {
+        const employee = {
+          name: "dm",
+          job: "software engineer",
+          years: 1,
+        };
+
+        delete employee.years;
+
+        const result = employee.years;
+        const guess = undefined;
+
+        expect(result).to.eql(guess);
+      });
+
+      it("중첩된 객체 다루기 #1", function () {
+        const result = {
+          name: "Vanilla Coding",
+          url: "https://www.vanillacoding.co/",
+          contact: {
+            email: "info@vanillacoding.co",
+            phone: "02-6713-7279",
+          },
+        };
+
+        const guess = {
+          name: "Vanilla Coding",
+          url: "https://www.vanillacoding.co/",
+        };
+
+        guess.contact = result.contact
+
+        expect(result).to.eql(guess);
+      });
+
+      it("중첩된 객체 다루기 #2", function () {
+        const company = {
+          name: "Vanilla Coding",
+          url: "https://www.vanillacoding.co/",
+          contact: {
+            email: "info@vanillacoding.co",
+            phone: "02-6713-7279",
+          },
+        };
+
+        const result = company.contact.email;
+        const guess = "info@vanillacoding.co";
+
+        expect(result).to.eql(guess);
+      });
+
+      it("중첩된 객체 다루기 #3", function () {
+        const reservation = {
+          name: "ken",
+          checkInDate: "2024-01-15",
+          checkOutDate: "2024-01-16",
+          location: {
+            country: "South Korea",
+            city: "Seoul",
+          },
+        };
+
+        const countries = ["South Korea", "Japan", "United States"];
+        const result = reservation.location[countries[0]];
+
+        const guess = undefined;
+        expect(result).to.eql(guess);
+      });
+
+      it("중첩된 객체 다루기 #4", function () {
+        const reservation = {
+          name: "ken",
+          checkInDate: "2024-01-15",
+          checkOutDate: "2024-01-16",
+          location: {
+            country: "South Korea",
+            city: "Seoul",
+          },
+        };
+
+        const result = reservation.location;
+        const key = "country";
+        delete reservation.location[key];
+
+        const guess = {
+          city: "Seoul"
+          };
+        expect(result).to.eql(guess);
+      });
+
+      it("객체와 함수 실행문", function () {
+        function sayHello() {
+          console.log("디자인을 합니다.");
+        }
+
+        const person = {
+          name: "dd",
+          job: "designer",
+          greeting: sayHello(),
+        };
+
+        const result = person.greeting;
+        const guess = undefined;
+
+        expect(result).to.eql(guess);
+      });
+
+      it("객체와 배열", function () {
+        const person = {
+          name: "ken",
+          job: "ceo",
+        };
+
+        person.interests = ["fishing", "coding"];
+        person.interests.push("marketing");
+
+        const result = person.interests[2];
+        const guess = "marketing";
+
+        expect(result).to.eql(guess);
+      });
+    });
+  });
+})();
